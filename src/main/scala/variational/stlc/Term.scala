c6119ceaa9cd823d1b915f9d099139e82d5c012f
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2011-10-05 12:55:42 +0200

    Added variational STLC terms and types as a base for case studies.

---
+++ src/main/scala/variational/stlc/Term.scala
@@ -0,0 +1,67 @@
+package variational.stlc
+
+import variational._
+
+/**
+ * Variational terms of the simply-typed lambda calculus.
+ *
+ * @author Tillmann Rendel
+ */
+
+trait Term extends VariationalContainer[Term.Structure, Term] {
+  def newChoice(condition : Int, that : Term) =
+    new Choice(condition, this, that) with ChoiceContainer[Term.Structure, Term] with Term
+}
+
+object Term {
+
+  implicit def liftTerm : Lift[Structure, Term] = new Lift[Structure, Term] {
+    def apply(value : Structure) =
+      value
+  }
+
+  trait Structure extends Term with StructureLike[Term] with SelfContainer[Structure, Term]
+
+  class Var(val identifier : String) extends Structure with Leaf[Term]
+
+  class App(val operator : Term, val operand : Term) extends Structure {
+    def all(f : VFunction1) =
+      App(f(operator), f(operand))
+
+    def all2(f : VFunction2, that : Term) =
+      if (that.isInstanceOf[App]) {
+        val other = that.asInstanceOf[App]
+        Some(App(f(operator, other.operator), f(operand, other.operand)))
+      } else {
+        None
+      }
+
+    def children = Seq(operator, operand)
+  }
+
+  object App {
+    def apply(operator : Term, operand : Term) =
+      new App(operator, operand)
+  }
+
+  class Abs(val identifier : String, val qualifier : Type, val body : Term) extends Structure {
+    def all(f : VFunction1) =
+      Abs(identifier, f(qualifier), f(body))
+
+    def all2(f : VFunction2, that : Abs#This) =
+      if (that.isInstanceOf[Abs]) {
+        val other = that.asInstanceOf[Abs]
+        Some(Abs(identifier, f(qualifier, other.qualifier), f(body, other.body)))
+      } else {
+        None
+      }
+
+    def children = Seq(qualifier, body)
+  }
+
+  object Abs {
+    def apply(identifier : String, qualifier : Type, body : Term) =
+      new Abs(identifier, qualifier, body)
+  }
+
+}
---
+++ src/main/scala/variational/stlc/Type.scala
@@ -0,0 +1,46 @@
+package variational.stlc
+
+import variational._
+
+/**
+ * Variational simple types.
+ *
+ * @author Tillmann Rendel
+ */
+
+trait Type extends VariationalContainer[Type.Structure, Type] {
+  def newChoice(condition : Int, that : Type) =
+    new Choice(condition, this, that) with ChoiceContainer[Type.Structure, Type] with Type
+}
+
+object Type {
+  implicit def liftType : Lift[Structure, Type] = new Lift[Structure, Type] {
+    def apply(value : Structure) =
+      value
+  }
+
+  trait Structure extends Type with StructureLike[Type] with SelfContainer[Structure, Type]
+
+  class Var(val identifier : String) extends Structure with Leaf[Type]
+
+  class Fun(val domain : Type, val codomain : Type) extends Structure {
+    def all(f : VFunction1) =
+      Fun(f(domain), f(codomain))
+
+    def all2(f : VFunction2, that : Type) =
+      if (that.isInstanceOf[Fun]) {
+        val other = that.asInstanceOf[Fun]
+        Some(Fun(f(domain, other.domain), f(codomain, other.codomain)))
+      } else {
+        None
+      }
+
+    def children = Seq(domain, codomain)
+  }
+
+  object Fun {
+    def apply(domain : Type, codomain : Type) =
+      new Fun(domain, codomain)
+  }
+
+}
