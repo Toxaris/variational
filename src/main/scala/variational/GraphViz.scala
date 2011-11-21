d97c40cac09d652a08b25bd539c8b59c7433df99
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2011-11-21 17:50:40 +0100

    Nicer graph output.

--- src/main/scala/variational/GraphViz.scala
+++ src/main/scala/variational/GraphViz.scala
@@ -19,6 +19,29 @@ class GraphViz(val variables : Array[String] = Array.empty) extends Memoize[Any,
 
   val builder = new StringBuilder()
 
+  val choiceFillColor = "#FF6700"
+  val choiceColor = "#CD5200"
+  val choiceFontName = "Calibri"
+  val choiceFontSize = "12"
+
+  val structureFillColor = "#909465" // "#94C600"
+  val structureColor = "#5F6142"
+  val structureFontName = "Calibri"
+  val structureFontSize = "12"
+
+  val thenEdgeFontName = "Calibri"
+  val thenEdgeFontSize = "12"
+  val thenEdgeColor = "black"
+  val thenEdgeStyle = "solid"
+
+  val elseEdgeFontName = "Calibri"
+  val elseEdgeFontSize = "12"
+  val elseEdgeColor = "black"
+  val elseEdgeStyle = "dotted"
+
+  val anyFontName = "Calibri"
+  val anyFontSize = "12"
+
   def result : String = builder.result
 
   def process(value : Any) = {
@@ -31,17 +54,47 @@ class GraphViz(val variables : Array[String] = Array.empty) extends Memoize[Any,
           if (c.condition < variables.length)
             variables(c.condition)
           else c.condition.toString
-        builder ++= "  " + node + " [label=\"" + condition + "?\", shape=diamond];\n"
+
+        // the node
+        builder ++= "  " + node + " ["
+        builder ++= "label=\"" + condition + "\", "
+        builder ++= "shape=\"box\", "
+        builder ++= "color=\"" + choiceColor + "\", "
+        builder ++= "fillcolor=\"" + choiceFillColor + "\", "
+        builder ++= "style=\"filled\","
+        builder ++= "fontname=\"" + choiceFontName + "\", "
+        builder ++= "fontsize=\"" + choiceFontSize + "\""
+        builder ++= "];\n"
 
         val thenBranchNode = apply(c.thenBranch)
-        builder ++= "  " + node + " -> " + thenBranchNode + "[label = \"yes\"];\n"
+        builder ++= "  " + node + " -> " + thenBranchNode + "["
+        builder ++= "style=\"" + thenEdgeStyle + "\","
+        builder ++= "color=\"" + thenEdgeColor + "\", "
+        builder ++= "fontname=\"" + thenEdgeFontName + "\", "
+        builder ++= "fontsize=\"" + thenEdgeFontSize + "\""
+        builder ++= "];\n"
 
         val elseBranchNode = apply(c.elseBranch)
-        builder ++= "  " + node + " -> " + elseBranchNode + "[label = \"no\"];\n"
+        builder ++= "  " + node + " -> " + elseBranchNode + "["
+        builder ++= "style=\"" + elseEdgeStyle + "\","
+        builder ++= "color=\"" + elseEdgeColor + "\", "
+        builder ++= "fontname=\"" + elseEdgeFontName + "\", "
+        builder ++= "fontsize=\"" + elseEdgeFontSize + "\""
+        builder ++= "];\n"
       }
 
       case s : StructureLike[_] => {
-        builder ++= "  " + node + " [label=\"" + s.prefix + "\", shape=box];\n"
+        builder ++= "  " + node
+        builder ++= " [label=\""
+        builder ++= s.prefix
+        builder ++= "\", "
+        builder ++= "color=\"" + structureColor + "\", "
+        builder ++= "fillcolor=\"" + structureFillColor + "\", "
+        builder ++= "fontname=\"" + structureFontName + "\", "
+        builder ++= "fontsize=\"" + structureFontSize + "\", "
+        builder ++= "style=\"filled\", "
+        builder ++= "shape=\"box\""
+        builder ++= "];\n"
 
         var i = 0
         for (child <- s.children) {
@@ -52,7 +105,11 @@ class GraphViz(val variables : Array[String] = Array.empty) extends Memoize[Any,
       }
 
       case any => {
-        builder ++= "  " + node + " [label=\"" + value + "\"];\n"
+        builder ++= "  " + node + " ["
+        builder ++= "label=\"" + value + "\","
+        builder ++= "fontname=\"" + anyFontName + "\", "
+        builder ++= "fontsize=\"" + anyFontSize + "\""
+        builder ++= "];\n"
       }
     }
 
