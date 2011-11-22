cc7100d3245c6a40a34eb1d4a75e889d90b04e5b
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2011-11-22 12:07:20 +0100

    Added jUnit XML listener for Jenkins.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,6 +3,6 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
     val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
     val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
-    // val junitXmlRepo = "Christoph's Maven Repo" at "http://maven.henkelmann.eu/"
-    // val junitXml = "eu.henkelmann" % "junit_xml_listener" % "0.2"
+    val junitXmlRepo = "Christoph's Maven Repo" at "http://maven.henkelmann.eu/"
+    val junitXml = "eu.henkelmann" % "junit_xml_listener" % "0.2"
 }
